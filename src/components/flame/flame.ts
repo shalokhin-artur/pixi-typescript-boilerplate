d1304787bdde5c9c0bd3e8a6795b58ec71e40cee
Artur Shalokhin <shalokhin.artur@gmail.com>
2022-12-25 16:05:57 +0300

    fix: flame

--- src/components/flame/flame.ts
+++ src/components/flame/flame.ts
@@ -2,26 +2,31 @@ import { Container, Ticker } from "pixi.js";
 import config from './config.json'
 import * as particles from '@pixi/particle-emitter';
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../../constants";
+import { Emitter } from "@pixi/particle-emitter";
 
 
 export default class Flame {
   readonly container = new Container()
   private ticker?: Ticker;
+  private emitter?: Emitter
 
   constructor() {}
 
   public play() {
-    const emitter = new particles.Emitter(this.container, config)
+    this.emitter = new particles.Emitter(this.container, config)
     const scale = 2
     this.container.scale.set(scale)
-    emitter.updateOwnerPos(SCREEN_WIDTH / 2 / scale, SCREEN_HEIGHT / 2 / scale);
+    this.emitter.updateOwnerPos(SCREEN_WIDTH / 2 / scale, SCREEN_HEIGHT / 2 / scale);
 
-    this.ticker = Ticker.shared.add((delta) => {
-      emitter.update(delta * 0.002)
-    })
+    if (this.emitter) {
+      this.ticker = Ticker.shared.add((delta) => {
+        this.emitter!.update(delta * 0.002)
+      })
+    }
   }
 
   public clear() {
+    this.emitter?.destroy()
     this.ticker?.stop()
     this.ticker?.destroy()
     this.container.removeChildren()
